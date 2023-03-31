d5069d888d07f708d304def305b30a5d9ac34a4d
Benjamin Hove <benhove86@gmail.com>
2023-03-31 15:12:01 -0400

    reverted back to button and switch conditions

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const total = document.getElementById("total")
 
 
 
+
 // percentage variable
 let tipPercentage = 0;
 
@@ -16,22 +17,44 @@ function percentageRate(rate) {
         case 5:
             tipPercentage = 0.05
             document.getElementById("five").style.backgroundColor = "var(--Strongcyan)";
+            document.getElementById("ten").style.backgroundColor = "var(--Verydarkcyan)";
+            document.getElementById("fifteen").style.backgroundColor = "var(--Verydarkcyan)";
+            document.getElementById("twentyfive").style.backgroundColor = "var(--Verydarkcyan)";
+            document.getElementById("fifty").style.backgroundColor = "var(--Verydarkcyan)";
+            
+    
             break;
         case 10:
             tipPercentage = 0.10
             document.getElementById("ten").style.backgroundColor = "var(--Strongcyan)";
+            document.getElementById("five").style.backgroundColor = "var(--Verydarkcyan)";
+            document.getElementById("fifteen").style.backgroundColor = "var(--Verydarkcyan)";
+            document.getElementById("twentyfive").style.backgroundColor = "var(--Verydarkcyan)";
+            document.getElementById("fifty").style.backgroundColor = "var(--Verydarkcyan)";
             break;
         case 15:
             tipPercentage = 0.15
             document.getElementById("fifteen").style.backgroundColor = "var(--Strongcyan)";
+            document.getElementById("five").style.backgroundColor = "var(--Verydarkcyan)";
+            document.getElementById("ten").style.backgroundColor = "var(--Verydarkcyan)";
+            document.getElementById("twentyfive").style.backgroundColor = "var(--Verydarkcyan)";
+            document.getElementById("fifty").style.backgroundColor = "var(--Verydarkcyan)";
             break;
         case 25:
             tipPercentage = 0.25 
             document.getElementById("twentyfive").style.backgroundColor = "var(--Strongcyan)";
+            document.getElementById("five").style.backgroundColor = "var(--Verydarkcyan)";
+            document.getElementById("fifteen").style.backgroundColor = "var(--Verydarkcyan)";
+            document.getElementById("ten").style.backgroundColor = "var(--Verydarkcyan)";
+            document.getElementById("fifty").style.backgroundColor = "var(--Verydarkcyan)";
             break;
         case 50:
             tipPercentage = 0.50
             document.getElementById("fifty").style.backgroundColor = "var(--Strongcyan)";
+            document.getElementById("five").style.backgroundColor = "var(--Verydarkcyan)";
+            document.getElementById("fifteen").style.backgroundColor = "var(--Verydarkcyan)";
+            document.getElementById("twentyfive").style.backgroundColor = "var(--Verydarkcyan)";
+            document.getElementById("ten").style.backgroundColor = "var(--Verydarkcyan)";
             break;
         case 100:
             tipPercentage = 0.50
